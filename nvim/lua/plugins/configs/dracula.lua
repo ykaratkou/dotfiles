edff4adbb8fa3cba0d07f0423cf2cfc694b0aaab
Evgeny Korotkov <evgn.korotkov@gmail.com>
2024-11-06 19:23:34 +0100

    Clean up drakula theme config. Tmux open link

--- nvim/lua/plugins/configs/dracula.lua
+++ nvim/lua/plugins/configs/dracula.lua
@@ -32,41 +32,5 @@ h('DiffDelete', { bg = colors.red, blend = 20 })
 h('DiffChange', { bg = colors.orange, blend = 20 })
 h('DiffText', { bg = colors.orange, blend = 25 })
 
--- Support Tmux dim
-local autocmd = vim.api.nvim_create_autocmd
-local augroup = vim.api.nvim_create_augroup
-
-autocmd('FocusLost', {
-  group = augroup('focus_lost', {}),
-  pattern = '*',
-  callback = function()
-    h('Normal', { fg = colors.fg, bg = colors.bg_dim })
-    h('SignColumn', { fg = colors.fg, bg = colors.bg_dim })
-    h('TelescopeNormal', { fg = colors.fg, bg = colors.bg_dim })
-    h('NeoTreeNormal', { fg = colors.fg, bg = colors.bg_dim })
-    h('NeoTreeNormalNC', { fg = colors.fg, bg = colors.bg_dim })
-    h('NvimTreeNormal', { fg = colors.fg, bg = colors.bg_dim })
-    h('NvimTreeNormalNC', { fg = colors.fg, bg = colors.bg_dim })
-    h('CmpItemAbbr', { fg = colors.white, bg = colors.bg_dim })
-    h('CmpItemAbbrMatch', { fg = colors.cyan, bg = colors.bg_dim })
-  end,
-})
-
-autocmd('FocusGained', {
-  group = augroup('focus_gained', {}),
-  pattern = '*',
-  callback = function()
-    h('Normal', { fg = colors.fg, bg = colors.bg })
-    h('SignColumn', { fg = colors.fg, bg = colors.bg })
-    h('TelescopeNormal', { fg = colors.fg, bg = colors.bg })
-    h('NeoTreeNormal', { fg = colors.fg, bg = colors.menu })
-    h('NeoTreeNormalNC', { fg = colors.fg, bg = colors.menu })
-    h('NvimTreeNormal', { fg = colors.fg, bg = colors.menu })
-    h('NvimTreeNormalNC', { fg = colors.fg, bg = colors.menu })
-    h('CmpItemAbbr', { fg = colors.white, bg = colors.bg })
-    h('CmpItemAbbrMatch', { fg = colors.cyan, bg = colors.bg })
-  end,
-})
-
 -- https://github.com/neovim/neovim/issues/23590
 vim.cmd('hi! link CurSearch Search')
