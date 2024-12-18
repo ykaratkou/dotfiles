33ebcc8d16a324640948af5f5f072671d395263c
Yauhen Karatkou <evgn.korotkov@gmail.com>
2024-12-18 16:13:51 +0100

    Update Copilot chat config

--- nvim/lua/plugins/configs/copilot_chat.lua
+++ nvim/lua/plugins/configs/copilot_chat.lua
@@ -1,4 +1,9 @@
 require("CopilotChat").setup({
+  window = {
+    layout = 'vertical',
+    width = 0.4,
+  },
+  highlight_selection = false,
 })
 
 vim.keymap.set('n', '<leader>go', ':CopilotChatToggle<CR>', { silent = true })
