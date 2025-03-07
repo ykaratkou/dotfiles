9a54fedb76cee2563249f77fbef28804344a6b0a
Yauhen Karatkou <evgn.korotkov@gmail.com>
2025-03-07 16:37:44 +0100

    Update Alacritty theme

--- fish/conf.d/themes.fish
+++ fish/conf.d/themes.fish
@@ -16,6 +16,8 @@ function update_theme --on-variable macos_theme
     --color=border:#44415a,header:#3e8fb0,gutter:#232136
     --color=spinner:#f6c177,info:#9ccfd8
     --color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
+
+    sed -i '' 's/rose_pine_dawn/rose_pine_moon/' ~/.dotfiles/alacritty.toml
   else if [ "$macos_theme" = "light" ]
     set -Ux FZF_DEFAULT_OPTS "
     --color=fg:#797593,bg:#faf4ed,hl:#d7827e
@@ -23,6 +25,8 @@ function update_theme --on-variable macos_theme
     --color=border:#dfdad9,header:#286983,gutter:#faf4ed
     --color=spinner:#ea9d34,info:#56949f
     --color=pointer:#907aa9,marker:#b4637a,prompt:#797593"
+
+    sed -i '' 's/rose_pine_moon/rose_pine_dawn/' ~/.dotfiles/alacritty.toml
   end
 end
 
