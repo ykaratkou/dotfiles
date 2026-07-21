f4af461c8f0632a942e9188c1db809f26b82538c
Yauhen Karatkou <evgn.korotkov@gmail.com>
2026-07-21 10:19:34 +0300

    docker compose aliases support

--- fish/conf.d/docker-compose.fish
+++ fish/conf.d/docker-compose.fish
@@ -1,2 +1,36 @@
-alias dweb="docker compose run --rm -it web"
-alias drdm="docker compose run --rm -it web rails db:migrate"
+alias dco='docker compose'
+function dcl
+  docker compose logs -f -n100 $argv[1]
+end
+
+function drun
+  if test (count $argv) -lt 2
+    echo "usage: drun <service> <alias> [args...]" >&2
+    return 1
+  end
+
+  set -l service $argv[1]
+  set -l name    $argv[2]
+  set -l extra   $argv[3..-1]
+
+  if not functions -q $name
+    echo "drun: '$name' is not a fish function/alias" >&2
+    return 1
+  end
+
+  set -l cmd (functions $name | string match -rg -- "--wraps='([^']*)'")
+  if test -z "$cmd"
+    echo "drun: could not resolve a command for '$name'" >&2
+    return 1
+  end
+
+  docker compose run --rm -it $service (string split ' ' -- $cmd) $extra
+end
+
+function dweb --description 'drun with service=web' --wraps drun
+    drun web $argv
+end
+
+function dharmony --description 'drun with service=web' --wraps drun
+    drun harmony $argv
+end
