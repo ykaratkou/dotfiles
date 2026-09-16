0e61925ecebc6a7219145a521820c1c3f78ca5e2
Yauhen Karatkou <evgn.korotkov@gmail.com>
2026-09-16 19:42:05 +0200

    Better git diff

--- nvim/lua/plugins/configs/blame.lua
+++ nvim/lua/plugins/configs/blame.lua
@@ -3,11 +3,7 @@ return {
     'FabijanZulj/blame.nvim',
     event = "VeryLazy",
     config = function()
-      require('blame').setup({
-        commit_detail_view = function(commit_hash, row, file_pat)
-          vim.cmd('CodeDiff ' .. commit_hash .. '^ ' .. commit_hash)
-        end
-      })
+      require('blame').setup()
 
       vim.keymap.set('n', '<leader>gb', ':BlameToggle<CR>')
     end,
--- nvim/lua/plugins/configs/codediff.lua
+++ nvim/lua/plugins/configs/codediff.lua
@@ -1,6 +1,20 @@
 vim.keymap.set('n', '<leader>gh', ':CodeDiff history %<CR>')
 vim.keymap.set('n', '<leader>gl', ':CodeDiff history<CR>')
 vim.keymap.set('n', '<leader>gd', ':CodeDiff<CR>')
+vim.keymap.set('x', '<leader>gd', function()
+  local commit_hash = vim.trim(table.concat(vim.fn.getregion(
+    vim.fn.getpos('v'),
+    vim.fn.getpos('.'),
+    { type = vim.fn.mode() }
+  ), '\n'))
+
+  if not commit_hash:match('^[0-9a-fA-F]+$') then
+    vim.notify('Select a Git commit SHA', vim.log.levels.ERROR)
+    return
+  end
+
+  vim.cmd('CodeDiff ' .. commit_hash .. '^ ' .. commit_hash)
+end, { desc = 'Diff selected commit against its parent' })
 
 vim.api.nvim_create_user_command('CodeDiffReview', function()
   vim.cmd('CodeDiff master... --inline')
